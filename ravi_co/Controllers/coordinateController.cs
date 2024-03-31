60d5cef3b470f831dccd72505e901f914769cbe5
D4-80565-PramitSamanta <63332503+D4-80565-PramitSamanta@users.noreply.github.com>
2024-03-31 10:51:11 +0530

    Made a Basic Model class and also made a end point to get a list of models

---
+++ ravi_co/Controllers/coordinateController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ravi_co.Models;
+
+namespace ravi_co.Controllers
+{
+    [Route("api/coord")]
+    [ApiController]
+    public class coordinateController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Coordinate> GetCos()
+        {
+            return new List<Coordinate>()
+            {
+                new Coordinate { id = 1, X = 10, Y = 20 },
+                new Coordinate { id = 2, X = 15, Y = 25 },
+                new Coordinate { id = 3, X = 20, Y = 30 }
+            };
+        }
+    }
+}
---
+++ ravi_co/Models/Coordinate.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ravi_co.Models
+{
+
+    public class Coordinate
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+    }
+}
